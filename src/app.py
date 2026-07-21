f7d2be7ab492be2bb51db922106d5075928eae22
Kayden <burnburnburn6@proton.me>
2026-07-21 12:39:49 -0400

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, render_template, url_for, redirect
+import psycopg2, hashlib, os
+
+app = Flask(__name__)
+
+def generate_token(url):
+    h = hashlib.sha256(url.encode()).hexdigest().upper()
+    return h[:6]
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            dbname=os.environ.get("POSTGRES_DB"),
+            user=os.environ.get("POSTGRES_USER"),
+            password=os.environ.get("POSTGRES_PASSWORD"),
+            host="sql_server"
+        )
+        return conn
+    except psycopg2.Error:
+        return None
+
+def init_db():
+    sql_query = """ CREATE TABLE IF NOT EXISTS url (
+    id SERIAL PRIMARY KEY,
+    token text NOT NULL UNIQUE,
+    redirect text NOT NULL
+    );"""
+
+    conn = None
+    cursor = None
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(sql_query)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        return "An error has occured", 500
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+@app.route('/', methods=['GET','POST'])
+def generate_url():
+    if request.method == 'POST':
+        conn = None
+        cursor = None
+        try:
+            conn = connect()
+            url = request.form['url']
+            if not url.startswith(('http://', 'https://')):
+                url = 'https://' + url
+            token = generate_token(url)
+            sql_query = """INSERT INTO url (token, redirect)
+                        VALUES (%s, %s) ON CONFLICT DO NOTHING;"""
+            cursor = conn.cursor()
+            cursor.execute(sql_query, (token, url))
+            conn.commit()
+            return f"New URL: {url_for('redirect_to_url', code=token, _external = True)}"
+        except Exception as e:
+            print(e)
+            return "An error has occured", 500
+        finally:
+            if cursor is not None:
+                cursor.close()
+            if conn is not None:
+                conn.close()
+    return render_template('index.html')
+
+@app.route('/<code>')
+def redirect_to_url(code):
+    conn = None
+    cursor = None
+    try:
+        conn = connect()
+        sql_query = """SELECT redirect FROM url WHERE token = %s;"""
+        cursor = conn.cursor()
+        cursor.execute(sql_query, (code,))
+        row = cursor.fetchone()
+
+        if row is None:
+            return "Redirect Not Found!", 404
+        return redirect(row[0])
+    except Exception as e:
+        print(e)
+        return "An error has occured", 500
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host="0.0.0.0", port=4242)
